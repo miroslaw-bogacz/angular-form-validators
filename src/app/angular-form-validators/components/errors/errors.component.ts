e0ffb338f0958fc5b32f196d32a9db423e99d772
miroslaw.bogacz <miroslaw.bogacz@hotmail.com>
2017-05-16 20:35:21 +0200

    add component to display validation errors

---
+++ src/app/angular-form-validators/components/errors/errors.component.ts
@@ -0,0 +1,47 @@
+import { Component, Input } from '@angular/core';
+import { AbstractControl } from '@angular/forms';
+
+@Component({
+  selector: 'afv-errors',
+  templateUrl: './errors.component.html',
+  styleUrls: [ './errors.component.css' ]
+})
+export class ErrorsComponent {
+
+  /**
+   * In this input you must use controls reference
+   *
+   * @type {AbstractControl}
+   */
+  @Input() public control: AbstractControl;
+
+  /**
+   * You can use this input to manipulate component visibility
+   *
+   * @type {boolean}
+   */
+  @Input() public show;
+
+  /**
+   * You can use values: dirty, pristin, touched, untouched
+   *
+   * @type {string}
+   */
+  @Input() public showByStatus: string;
+
+  constructor() { }
+
+  public errors(control): Array<string> {
+    return Object.keys(control.errors)
+      .reduce((acc: Array<string>, value: string) => acc.concat(control.getError(value)), []);
+  }
+
+  public get isShow(): boolean {
+    const status: boolean = this.control[this.showByStatus];
+
+    return this.show  && this.showByStatus === undefined
+        || this.show === undefined && status
+        || this.show && status;
+  }
+
+}
